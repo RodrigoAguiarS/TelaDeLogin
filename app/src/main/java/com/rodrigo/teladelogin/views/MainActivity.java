32a31a09e041984b977aebf1bf2fb0527787dd40
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-10-13 11:56:50 -0300

    Initial commit - Criação da Tela do xml

---
+++ app/src/main/java/com/rodrigo/teladelogin/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rodrigo.teladelogin.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rodrigo.teladelogin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
